5e8e91f8e4be8ea585e9a3e5e0712674f5c4da14
Chris Roberts <chrisroberts.code@gmail.com>
2013-09-27 06:41:25 -0700

    Update development version. Loosen dependency version restrictions

--- metadata.rb
+++ metadata.rb
@@ -3,12 +3,11 @@ description      "Install and configure kafka"
 maintainer       "Heavy Water Ops, LLC"
 maintainer_email "support@hw-ops.com"
 license          "Apache 2.0"
-version          "0.1.0"
+version          "0.1.1"
 
 supports "ubuntu"
 
-depends "java",  "~> 1.11.4"
-depends "runit", "~> 1.1.4"
-depends "builder", "~> 0.1.1"
-depends "zookeeperd", "~> 0.1.0"
-
+depends "java",  ">= 1.11.4"
+depends "runit", ">= 1.1.4"
+depends "builder", ">= 0.1.1"
+depends "zookeeperd", ">= 0.1.0"
