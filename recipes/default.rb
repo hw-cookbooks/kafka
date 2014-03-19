b088d110bddc5167dfae11727e8336c2d705ee57
Ian D. Rossi <ian.d.rossi@aimtheory.com>
2014-03-19 07:31:56 -1000

    Added separate kafka::zookeeper recipe so zookeeper can be a separate node. Modified README.md to reflect Centos 6.4 support

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,8 +16,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-include_recipe "zookeeperd::server"
-
 ::Chef::Recipe.send(:include, ChefKafka::Helpers)
 
 node.default[:kafka][:config]["#{kafka_zk_prefix}.connectiontimeout.ms"] = 1000000
