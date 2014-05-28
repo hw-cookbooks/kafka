7e9dd2a4ac10b00ee1c96dae860f34804b22583f
Michael F. Weinberg <michael@luckymike.com>
2014-05-28 14:17:34 -0700

    Limit broker id to 16 bit signed integer for 0.7.x because larger integers cause a java integer overflow. Use platform families in metadata. Indicate that we support RHEL family too!

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,8 @@ maintainer_email "support@hw-ops.com"
 license          "Apache 2.0"
 version          "0.2.3"
 
-supports "ubuntu"
+supports "debian"
+supports "rhel"
 
 depends "java",  ">= 1.11.4"
 depends "runit", ">= 1.1.4"
