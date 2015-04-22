3d217dcd9c9b98bb8534f03dd078f767a96e972d
Aaron Baer <aaron@hw-ops.com>
2015-04-22 11:51:19 -0700

    correct build_commands error when checking versions for installation. rename methods for clarification.

--- libraries/chef_kafka.rb
+++ libraries/chef_kafka.rb
@@ -5,7 +5,7 @@ module ChefKafka
     def kafka_download_url(version)
       uri = nil
 
-      if kafka_is_below_07?
+      if kafka_is_07?
         uri = URI.parse("http://archive.apache.org")
         uri.path = %W{
           /dist/kafka/old_releases
@@ -37,20 +37,20 @@ module ChefKafka
     #
     # @return [TrueClass, FalseClass] whether or not the version is equal or
     #   less than 0.7.x
-    def kafka_is_below_07?
+    def kafka_is_07?
       bits = node[:kafka][:version].split('.')
       bits[0].to_i == 0 && bits[1].to_i <= 7
     end
 
     # version 0.8.1 and above uses gradle to build 0.8.0 and lower use sbt
     # this checks valid version for setting up build_commands
-    def kafka_is_above_081?
+    def kafka_is_081?
       bits = node[:kafka][:version].split('.')
       bits[0].to_i == 0 && bits[1].to_i >= 8 && bits[2].to_i > 0 && bits[2].to_i < 2
     end
 
     # version 0.8.2 and above does not ship gradle
-    def kafka_is_above_082?
+    def kafka_is_082?
       bits = node[:kafka][:version].split('.')
       bits[0].to_i == 0 && bits[1].to_i >= 8 && bits[2].to_i >= 2
     end
@@ -60,7 +60,7 @@ module ChefKafka
     #
     # @return [String] the correct ZooKeeper prefix name
     def kafka_zk_prefix
-      kafka_is_below_07? ? "zk" : "zookeeper"
+      kafka_is_07? ? "zk" : "zookeeper"
     end
 
     # Returns the correct broker key name based on the desired version of
@@ -68,14 +68,14 @@ module ChefKafka
     #
     # @return [String] the correct broker key name
     def kafka_broker_key
-      kafka_is_below_07? ? "brokerid" : "broker.id"
+      kafka_is_07? ? "brokerid" : "broker.id"
     end
 
     # Calculates a new broker id that won't overflow a Java Integer.
     #
     # @return [Integer] a new broker id
     def new_kafka_broker_id
-      int_bit = kafka_is_below_07? ? 15 : 31
+      int_bit = kafka_is_07? ? 15 : 31
       max_java_integer = (2 ** int_bit) - 1
       %x{hostid}.to_i(16) % max_java_integer
     end
