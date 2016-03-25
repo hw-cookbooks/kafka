d9a718e556358496ebe3771dc5201b250ad236ea
Aaron Baer <aaron@hw-ops.com>
2016-03-25 15:23:36 -0700

    update contact information and source_url and issues_url entry

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,11 @@
 name             "kafka"
 description      "Install and configure kafka"
 maintainer       "Heavy Water Operations, LLC"
-maintainer_email "support@hw-ops.com"
+maintainer_email "helpdesk@heavywater.io"
 license          "Apache 2.0"
 version          "0.2.7"
+source_url        "https://github.com/hw-cookbooks/kafka" if respond_to?(:source_url)
+issues_url        "https://github.com/hw-cookbooks/kafka/issues" if respond_to?(:issues_url)
 
 supports "debian"
 supports "rhel"
